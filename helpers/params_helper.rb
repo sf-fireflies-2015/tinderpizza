02b2cc883a0c522feac1ef0e4e823121a09468fb
Stujo <stujo@github.com>
2015-08-26 11:52:50 -0700

    Removing unnecessary variable

--- helpers/params_helper.rb
+++ helpers/params_helper.rb
@@ -1,7 +1,7 @@
 helpers do
 
   def params_whitelist(allowed_keys)
-    selected = params.select do |key, value|
+    params.select do |key, value|
       allowed_keys.include? key.to_sym
     end
   end
